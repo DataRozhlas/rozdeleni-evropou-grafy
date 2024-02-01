8eb9fbd7a2e678706b170c4bfe693b62bbc0e0a2
Petr Kočí <petr.koci@samizdat.cz>
2024-02-01 12:45:44 +0100

    process data

---
+++ index.ts
@@ -0,0 +1,19 @@
+import * as XLSX from "xlsx";
+
+const data = XLSX.readFile(
+  "./srv/data/TABULKY_STEM_pro_CRo_hlavni_soubor.xls",
+  { dense: true },
+);
+
+const qs = data.Sheets["Celá veřejnost 18+"].reduce((acc, row, i) => {
+  if (i > 2 && i < 549 && row[0]) acc = [...acc, { q: row[0].v, a: [row.slice(1,9).map((a) => a.v)] }];
+  if (i > 2 && i < 549 && row[0]=== undefined && row[1].v !== "Počet") {
+    
+    acc[acc.length-1].a.push(row.slice(1, 9).map((a) => a.v));
+  }
+  return acc;
+}, []);
+
+console.log(qs);
+
+Bun.write("./srv/data/qas.json", JSON.stringify(qs, null, 2));
